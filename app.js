9dbeea9c7189e58446b02636898d46dfd48f80e1
alibek <alibekbekkaliev6@gmail.com>
2026-01-30 14:37:58 +0500

    fix3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
-import itemsRouter from ".items.js"; // Import routes
+import itemsRouter from "./routes/items"; // Import routes
 
 dotenv.config();
 
